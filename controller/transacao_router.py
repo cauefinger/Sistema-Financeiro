a447ed2edb23cfece324af6461172e040d458e53
Caue Finger <fingercaue@gmail.com>
2026-08-31 16:06:07 -0300

    verificar token adicionado na rota de transação

--- controller/transacao_router.py
+++ controller/transacao_router.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from controller.Depends import verificar_token
+from controller import Depends
 transacao_router = APIRouter (
     prefix= "/transacoes",
     tags=["trasação"]
 )
 
 @transacao_router.get("/")
-async def mensagem_rota():
-    return {"mensagem":"Você entrou na rota de transações."}   
+
+async def entrar_rota(
+        entrar_rota = Depends(verificar_token)
+        ):
+        return {"mensagem": "Você está autenticado e entrou na rota de transações."}
+    
