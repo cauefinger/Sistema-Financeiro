ba79bbfb8b153f83e61d2561eaa1929294d244f9
Caue Finger <fingercaue@gmail.com>
2026-09-09 18:39:36 -0300

    criação da rota de categoria e da categoria service

---
+++ service/Categoria_service.py
@@ -0,0 +1,9 @@
+from controller.Depends import pegar_sessao, Session, verificar_token
+from controller import Depends
+from model import Categoria
+from controller.auth_router import Usuario
+def buscar_todas_categorias(
+        sessao: Session = Depends(pegar_sessao),
+        usuario_atual: Usuario = Depends(verificar_token)
+):
+    categorias = sessao.query(Categoria).filter(usuario_atual.id == Usuario.id).all()
