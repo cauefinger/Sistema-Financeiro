021edd7e6c0ed0a89ae318e81f3f40751356822d
Caue Finger <fingercaue@gmail.com>
2026-09-11 18:39:41 -0300

    verificar token adicionado na rota de categorias

--- controller/categoria_router.py
+++ controller/categoria_router.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter
 from service.Categoria_service import buscar_todas_categorias
+from controller.Depends import Depends, pegar_sessao, Session, verificar_token
+
 categoria_router = APIRouter (
     prefix= "/categorias",
     tags=["categorias"]
 )
 
 @categoria_router.get("/")
-async def mensagem_rota():
-    return {"mensagem":"Você entrou na rota de categoria."}   
-    return buscar_todas_categorias
+async def mensagem_rota(
+    sessao: Session = Depends(pegar_sessao),
+    usuario_atual: dict = Depends(verificar_token)
+): 
+    categrias = buscar_todas_categorias()
+    return {"mensagem":"Você entrou na rota de categoria.",
+            "categorias": categrias}   
+    
--- service/Categoria_service.py
+++ service/Categoria_service.py
@@ -1,13 +1,13 @@
 from controller.Depends import pegar_sessao, Session, verificar_token
 from controller import Depends
 from model import Categoria
-from controller.auth_router import Usuario
+from controller.auth_router import Usuario, Depends
 
 
 def buscar_todas_categorias(
         sessao: Session = Depends(pegar_sessao),
         usuario_atual: Usuario = Depends(verificar_token)
 ):
-    categorias = sessao.query(Categoria).filter(usuario_atual.id == Usuario.id).all()
+        categorias = sessao.query(Categoria).filter(usuario_atual.id == Usuario.id).all()
 
-    return categorias
+        return categorias
