ecb93341f837e5f548898bab316e708dc4ab264a
Caue Finger <fingercaue@gmail.com>
2026-09-12 18:35:17 -0300

    Rota de categoria funcionando

--- controller/categoria_router.py
+++ controller/categoria_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from service.Categoria_service import buscar_todas_categorias
 from controller.Depends import pegar_sessao, Session, verificar_token
+from controller.auth_router import Usuario
 
 categoria_router = APIRouter (
     prefix= "/categorias",
@@ -10,9 +11,12 @@ categoria_router = APIRouter (
 @categoria_router.get("/")
 async def mensagem_rota(
     sessao: Session = Depends(pegar_sessao),
-    usuario_atual: dict = Depends(verificar_token)
+    usuario_atual: Usuario = Depends(verificar_token)
 ): 
-    categrias = buscar_todas_categorias()
+    categrias = buscar_todas_categorias(
+        sessao,
+        usuario_atual
+    )
     return {"mensagem":"Você entrou na rota de categoria.",
             "categorias": categrias}   
     
--- service/Categoria_service.py
+++ service/Categoria_service.py
@@ -1,6 +1,6 @@
-from controller.Depends import pegar_sessao, verificar_token
+from controller.Depends import pegar_sessao, Session, verificar_token
 from fastapi import Depends
-from model import Categoria
+from model.Categoria import Categoria
 from controller.auth_router import Usuario
 
 
@@ -9,6 +9,6 @@ def buscar_todas_categorias(
         sessao: Session,
         usuario_atual: Usuario = Depends(verificar_token)
 ):
-        categorias = sessao.query(Categoria).filter(Categoria.usuario_id == Usuario.id).all()
+        categorias = sessao.query(Categoria).filter(Categoria.usuario_id == usuario_atual.id).all()
 
         return categorias
